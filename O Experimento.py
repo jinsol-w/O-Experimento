6ed47b0303d51ff41f19e77957cb0c93a07206c5
Carlos Morgado <carlos.morgado@sempreceub.com>
2026-06-16 19:28:42 -0300

    História Inicial

--- O Experimento.py
+++ O Experimento.py
@@ -2,3 +2,43 @@ import time
 import random
 
 nome = input("Digite seu nome ou apelido")
+
+print("=== O Experimento ===")
+
+print("Ei!\n")
+
+print("Acorde", nome)
+
+print("Você: Onde estou?")
+
+print("Nós te sequestramos. Somos do governo e queremos realizar uma seleção nos cidadãos.")
+
+print("Você: O que??? Como assim? O que isso quer dizer?")
+
+print("Nós perdemos a fé na humanidade. A sociedade tem ficado cada vez mais burra...")
+
+print("Eu me chamo Nefário e conduzirei o experimento.")
+
+print("Você receberá perguntas diversas de conhecimentos básicos que todos deveriam ter.")
+
+print("Você precisa acertar no MÍNIMO... 8 para sobreviver.")
+
+print("Você: Sobreviver? Como assim?")
+
+print("Sim. Caso você obtenha um desempenho menor que 8/10...")
+
+print("Você...")
+
+print("Será...")
+
+print("Descartado!")
+
+print("Você: Espere, descartado? O que isso quer dizer?")
+
+print("Que você será 'desvivido'.")
+
+print("Você: O QUÊ!? ESPERA!")
+
+print("Boa sorte!")
+
+print("Você: Não! Calma!")
